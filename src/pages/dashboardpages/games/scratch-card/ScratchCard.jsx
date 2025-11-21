3eb26e6ed03dfe822c5da6effa5324beccf5c104
nabishafin <shafin21215@gmail.com>
2025-11-22 00:44:21 +0600

    Refactor dashboard pages: - Remove EditAbout, EditProfile, EditPrivacyPolicy, EditTermsAndConditions, AllNotifications, Profile, AllUsers components. - Add new components: Games, ScratchCard, SpinWheel, History, Home, Language, Wallet.

---
+++ src/pages/dashboardpages/games/scratch-card/ScratchCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ScratchCard = () => {
+  return <div>ScratchCard</div>;
+};
+
+export default ScratchCard;
--- src/pages/dashboardpages/notification/AllNotifications.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AllNotifications = () => {
-  return <div>AllNotifications</div>;
-};
-
-export default AllNotifications;
